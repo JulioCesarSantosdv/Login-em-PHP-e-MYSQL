97ca83dee4e164d332c17e2461bb62e5cf6f19e4
JulioCesarSantosdv <jc-santos-2@hotmail.com>
2024-07-29 22:52:08 -0300

    ajuste do css

--- pages/cadastrar.php
+++ pages/cadastrar.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <?php
-// Inclui a classe User
-require_once '../CLASS/users.php'; 
-// Cria uma nova instância da classe User
+require_once '../CLASS/users.php';
 $u = new User(); 
 ?>
 <html lang="pt-BR">
@@ -13,12 +11,10 @@ $u = new User();
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    <!-- Vídeo de fundo -->
     <video autoplay muted loop id="bg-video">
         <source src="../video/fundo6.mp4" type="video/mp4">
         Seu navegador não suporta a tag de vídeo.
     </video>
-    <!-- Formulário de cadastro -->
     <div id="form-body-cad">
         <h1>Cadastrar</h1>
         <form method="POST" action="process.php">
@@ -31,23 +27,17 @@ $u = new User();
         </form>
     </div>
     <?php
-    // Verifica se o formulário foi enviado
     if (isset($_POST['nome'])) {
-        // Sanitiza os inputs do usuário
         $nome = htmlspecialchars($_POST['nome']);
         $telefone = htmlspecialchars($_POST['telefone']);
         $email = htmlspecialchars($_POST['email']);
         $senha = htmlspecialchars($_POST['senha']);
         $confirmarSenha = htmlspecialchars($_POST['confSenha']);
 
-        // Verifica se todos os campos estão preenchidos
         if (!empty($nome) && !empty($telefone) && !empty($email) && !empty($senha) && !empty($confirmarSenha)) {
-            // Conecta ao banco de dados
             $u->conectar("projeto_login", "localhost", "root", "");
-            if ($u->msgErro == "") { // Sem erro na conexão
-                // Verifica se as senhas coincidem
+            if ($u->msgErro == "") {
                 if ($senha == $confirmarSenha) {
-                    // Tenta cadastrar o usuário
                     if ($u->cadastrar($nome, $telefone, $email, $senha)) {
                         echo "Cadastrado com sucesso, acesse para entrar!";
                     } else {
@@ -65,7 +55,6 @@ $u = new User();
     }
     ?>
     <script>
-            // Esconde a mensagem após 2 segundos
         setTimeout(function() {
             var message = document.getElementById('message');
             if (message) {
